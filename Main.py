edd4ca6782b122a2ba6b6980a75da310341a70dd
adamnapieralski <adam.napieralski@gmail.com>
2019-05-08 21:05:11 +0200

    Using least_squares method to calculate tags positions and saving it

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
+from sympy import Symbol, nsolve
+from scipy.optimize import least_squares
+import numpy as np
+
+
 anchors = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
+
 
 with open("python_zadanie_2.txt", "r") as task_file:
     task_file_lines = task_file.readlines()
@@ -19,3 +25,37 @@ for line in task_file_lines[task_file_lines.index('2. Pomiary odległości obiek
         break
 
 print(tag2anchs_dist)
+
+def equations(X_vec, **kwargs):
+    F = []
+    for i in range(4):
+        F.append((kwargs['anchors'][i][0] - X_vec[0]) ** 2 + (kwargs['anchors'][i][1] - X_vec[1]) ** 2
+                 - kwargs['distances'][i] ** 2)
+    return np.array(F)
+
+def getTagPosition(anchors, distances):
+    X_vec_0 = np.array([1, 1])
+    tag_data = {'anchors': anchors, 'distances': distances}
+    res1 = least_squares(equations, X_vec_0, method='lm', kwargs=tag_data)
+    return tuple(res1.x)
+
+# def getTagPositionNSolve(anchors, distances):
+#     x, y = Symbol('x'), Symbol('y')
+#     F = []
+#     for i in range(4):
+#         F.append((anchors[i][0] - x) ** 2 + (anchors[i][1] - y) ** 2 - distances[i] ** 2)
+#     prim_approx = [0, 0]
+#     ans = nsolve(F, [x, y], prim_approx, verify=False)
+#     for i in range(4):
+#         print((anchors[i][0] - ans[0]) ** 2 + (anchors[i][1] - ans[1]) ** 2 - distances[i] ** 2)
+#
+#     print('\n')
+#     return tuple(ans)
+
+# for i in range(4):
+#     print((anchors[i][0] - res1.x[0]) ** 2 + (anchors[i][1] - res1.x[1]) ** 2 - distances[i] ** 2)
+
+tags_data = []
+for el in tag2anchs_dist:
+    tags_data.append([el[0], getTagPosition(anchors, el[1:])])
+print(tags_data)

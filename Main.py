c732e8538f75c419ce41789ebda727c8a9a34f90
adamnapieralski <adam.napieralski@gmail.com>
2019-05-08 16:02:47 +0200

    Reading from file and storing in a list tags with distances to anchores

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
-print("cos")
+anchors = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
+
+with open("python_zadanie_2.txt", "r") as task_file:
+    task_file_lines = task_file.readlines()
+
+tag2anchs_dist = []
+
+blank_count = 0;
+for line in task_file_lines[task_file_lines.index('2. Pomiary odległości obiektów od anchorów\n') + 6:]:
+    if line != '\n':
+        str_line = line.strip('\n').split(' ')
+        num_lin = [float(i) for i in str_line]
+        num_lin[0] = int(num_lin[0])
+        tag2anchs_dist.append(num_lin)
+        blank_count = 0;
+    else:
+        blank_count += 1
+    if blank_count > 1:
+        break
+
+print(tag2anchs_dist)
